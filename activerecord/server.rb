a24baa5ee1db3a0971230076faa239ad1060d88e
Morgan Fox <morganfox61297@gmail.com>
2019-07-03 14:18:51 -0400

    user logins

--- activerecord/server.rb
+++ activerecord/server.rb
@@ -1,3 +1,67 @@
+require "sinatra"
 require "sinatra/activerecord"
 
-set :database, {adapter: "sqlite3", database: "./database.sqlite3"}
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: "./database.sqlite3")
+
+#set :database, {adapter: "sqlite3", database: "./database.sqlite3"}
+
+enable :sessions
+
+class User < ActiveRecord::Base
+end
+
+get '/' do
+  puts "running"
+  erb :home
+end
+
+get '/signup' do
+  @user = User.new
+  erb :'users/signup'
+  if session[:user_id]
+    redirect "/"
+  else
+    erb :'users/signup'
+  end
+end
+
+post '/signup' do
+  @user = User.new(params)
+  if @user.save
+    p "#{@user.first_name} was saved to the database"
+    redirect "/thanks"
+  end
+end
+
+get "/thanks" do
+  erb :'users/thanks'
+end
+
+get "/login" do
+  erb :'users/login'
+  if session[:user_id]
+    redirect "/"
+  else
+    erb :'users/login'
+  end
+end
+
+post "/login" do
+  given_password = params['password']
+  user = User.find_by(email: params['email'])
+  if user
+     if user.password ==  given_password
+       p "User authenticated successfully"
+       session[:user_id] = user.id
+     else
+       p "Invalid email or password"
+     end
+  end
+end
+
+#DELETE request
+post '/logout' do
+  session.clear
+  p "user logged out successfully"
+  redirect "/"
+end
